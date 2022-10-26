5f95dd931c5a6be1d7f50af57808952a868b1640
Christopher Belanger <christopher@belangeranalytics.com>
2022-10-26 09:37:35 -0400

    Bugfixing daily consolidations
    * Rentals.ca use correct bathrooms column
    * Kijiji filtering on rent as numeric, instead of incorrectly as character
    * Add stop() for cleaner error-outs if tryCatch()s fail
    * Add backup padmapper titles using address, if no title given
    * Move rent filtering to last steps, to catch errors in all sources
    * Remove vacant land
    * Change property_type NA to "unspecified"

--- code/00.ingestion/R/consolidate_daily.R
+++ code/00.ingestion/R/consolidate_daily.R
@@ -88,6 +88,7 @@ table_read <- tryCatch ({
 
 if ("error" %in% class(table_read)){
   logdriver::add_log(level = "critical", event = "Consolidation: Failed reading from database.", username = username)
+  stop("Consolidation: Failed reading from database.")
 } else {
   logdriver::add_log(level = "info", event = "Consolidation: Success reading from database.", username = username)
 }
@@ -98,81 +99,88 @@ if ("error" %in% class(table_read)){
 
 cleandata_try <- tryCatch({
 
-### rentals.ca ----
+  ### rentals.ca ----
 
-rentalsca_clean <- rn %>%
-  dplyr::mutate(unique_id = sprintf("rn-%s-%s", location_id, id)) %>%
-  dplyr::select(title, property_type, bedrooms = beds, rent, address = address1, lat, lon = lng, unique_id, url) %>%
-  dplyr::mutate(source = "rentals.ca") %>%
-  add_nad_sf() %>%
-  dplyr::mutate(dplyr::across(-"geometry", as.character))
-
-
-### realtor.ca ----
-
-realtorca_clean <- re %>%
-  mutate(unique_id = sprintf("rl-%s-%s",mls_number, id)) %>%
-  mutate(url = paste0("https://www.realtor.ca", relative_url_en)) %>%
-  select(title = public_remarks, property_type, bedrooms = building_bedrooms,
-         bathrooms = building_bathroom_total, rent = property_lease_rent_unformatted_value, address = property_address_address_text,
-         lat = property_address_latitude, lon = property_address_longitude, unique_id, url) %>%
-  mutate(title = strtrim(title, width = 100),
-         bedrooms = as.numeric(stringr::str_extract(bedrooms, "\\d")),
-         source = "realtor.ca") %>%
-  add_nad_sf() %>%
-  dplyr::mutate(dplyr::across(-"geometry", as.character))
+  rentalsca_clean <- rn %>%
+    dplyr::mutate(unique_id = sprintf("rn-%s-%s", location_id, id)) %>%
+    dplyr::select(title, property_type, bedrooms, bathrooms, rent, address = address1,
+                  lat, lon = lng, unique_id, url, date_scraped) %>%
+    dplyr::mutate(source = "rentals.ca") %>%
+    add_nad_sf() %>%
+    dplyr::mutate(dplyr::across(-"geometry", as.character))
 
 
-### padmapper ----
+  ### realtor.ca ----
 
-if (!"url" %in% colnames(pm)){
-
-  pm <- pm %>%
-    dplyr::mutate(url = dplyr::if_else(
-      is.na(pb_id),
-      paste0("https://www.padmapper.com/apartments/",pl_id,"p"),
-      paste0("https://www.padmapper.com/buildings/p", pb_id)
-    ))
-}
-
-padmapper_clean <- pm %>%
-  mutate(unique_id = sprintf("pm-%s-%s", building_id, listing_id)) %>%
-  select(title = building_name, bedrooms, bathrooms = bathrooms, rent = price,
-         address = address, lat = lat, lon = lng, unique_id, url) %>%
-  mutate(title = strtrim(title, width = 100),
-         source = "padmapper") %>%
-  add_nad_sf() %>%
-  dplyr::mutate(dplyr::across(-"geometry", as.character))
+  realtorca_clean <- re %>%
+    mutate(unique_id = sprintf("rl-%s-%s",mls_number, id)) %>%
+    mutate(url = paste0("https://www.realtor.ca", relative_url_en)) %>%
+    select(title = public_remarks, property_type, bedrooms = building_bedrooms,
+           bathrooms = building_bathroom_total, rent = property_lease_rent_unformatted_value, address = property_address_address_text,
+           lat = property_address_latitude, lon = property_address_longitude, unique_id, url, date_scraped) %>%
+    mutate(title = strtrim(title, width = 100),
+           bedrooms = as.numeric(stringr::str_extract(bedrooms, "\\d")),
+           source = "realtor.ca") %>%
+    add_nad_sf() %>%
+    dplyr::mutate(dplyr::across(-"geometry", as.character))
 
 
+  ### padmapper ----
 
-### Kijiji ----
+  if (!"url" %in% colnames(pm)){
 
-kijiji_clean <- kj %>%
-  mutate(unique_id  = paste0("kj-", stringr::str_extract(url, "(?<=/)\\d+$"))) %>%
-  select(title, bedrooms, bathrooms, property_type = unit_type,
-         rent = price, address = address, lat = lat, lon = lon, unique_id, url) %>%
-  distinct(title, bedrooms, .keep_all = TRUE) %>%
-  mutate(bedrooms = dplyr::if_else(stringr::str_detect(bedrooms, "Bachelor"), "0", bedrooms),
-         bedrooms = as.numeric(stringr::str_extract(bedrooms, "\\d"))) %>%
-  mutate(source = "kijiji") %>%
-  filter(rent > 0,
-         rent < 10000) %>%
-  add_nad_sf() %>%
-  dplyr::mutate(dplyr::across(-"geometry", as.character))
+    pm <- pm %>%
+      dplyr::mutate(url = dplyr::if_else(
+        is.na(pb_id),
+        paste0("https://www.padmapper.com/apartments/",pl_id,"p"),
+        paste0("https://www.padmapper.com/buildings/p", pb_id)
+      ))
+  }
 
+  # padmapper does not always give titles, so if there is no title we give the address
+  padmapper_clean <- pm %>%
+    dplyr::mutate(unique_id = sprintf("pm-%s-%s", building_id, listing_id)) %>%
+    dplyr::select(title = building_name, bedrooms, bathrooms = bathrooms, rent = price,
+                  address = address, lat = lat, lon = lng, unique_id, url, date_scraped) %>%
+    dplyr::mutate(title = strtrim(title, width = 100),
+                  source = "padmapper") %>%
+    dplyr::mutate(title = dplyr::if_else(is.na(title), address, title)) %>%
+    add_nad_sf() %>%
+    dplyr::mutate(dplyr::across(-"geometry", as.character))
+
+
+
+  ### Kijiji ----
+
+  kijiji_clean <- kj %>%
+    mutate(unique_id  = paste0("kj-", stringr::str_extract(url, "(?<=/)\\d+$"))) %>%
+    select(title, bedrooms, bathrooms, property_type = unit_type,
+           rent = price, address = address, lat = lat, lon = lon, unique_id, url, date_scraped) %>%
+    distinct(title, bedrooms, .keep_all = TRUE) %>%
+    mutate(bedrooms = dplyr::if_else(stringr::str_detect(bedrooms, "Bachelor"), "0", bedrooms),
+           bedrooms = as.numeric(stringr::str_extract(bedrooms, "\\d"))) %>%
+    mutate(source = "kijiji") %>%
+    add_nad_sf() %>%
+    dplyr::mutate(dplyr::across(-"geometry", as.character))
+
+  TRUE
 }, error = function(e) {print(e); e})
 
 
 if ("error" %in% class(cleandata_try)){
   logdriver::add_log(level = "critical", event = "Consolidation: Failed to clean data.", username = username)
+  stop("Consolidation: Failed to clean data.")
 } else {
   logdriver::add_log(level = "info", event = "Consolidation: Success cleaning data.", username = username)
 }
 
 ## Consolidate ----
 
-# we start with rentals.ca data
+# we start with rentals.ca data, then add sources one by one, keeping only results
+# that are not within 50m of a previous result to avoid duplicates.
+# we clean each data source individually to remove within-source duplicates
+# then rely on geographic boundaries to avoid inter-source duplicates
+# we also filter out vacant land, and rents under 10$ and over 10,000$
 
 consolidation <- tryCatch({
   rentalsca_clean %>%
@@ -181,13 +189,22 @@ consolidation <- tryCatch({
     remove_geo_overlaps(kijiji_clean) %>%
     sf::st_join(ons_shp_gen2) %>%
     sf::st_join(ons_shp_gen3) %>%
-    sf::st_set_geometry(NULL)
+    sf::st_set_geometry(NULL) %>%
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.character),
+      bedrooms = as.numeric(bedrooms),
+                  bathrooms = as.numeric(bathrooms),
+                  rent = as.numeric(rent)) %>%
+    dplyr::filter(!property_type %in% "Vacant Land") %>%
+    dplyr::mutate(property_type = dplyr::if_else(is.na(property_type), "unspecified", tolower(property_type))) %>%
+    dplyr::filter(as.numeric(rent) > 10,
+           as.numeric(rent) < 10000)
 
 }, error = function(e) {print(e); e})
 
 
 if ("error" %in% class(consolidation)){
   logdriver::add_log(level = "critical", event = "Consolidation: Failed combine data sources.", username = username)
+  stop("Consolidation: Failed combine data sources.")
 } else {
   logdriver::add_log(level = "info", event = "Consolidation: Success combining data sources.", username = username)
 }
@@ -208,6 +225,7 @@ table_write <- tryCatch ({
 
 if ("error" %in% class(table_write)){
   logdriver::add_log(level = "critical", event = "Consolidation: Failed to write data to database.", username = username)
+  stop("Consolidation: Failed to write data to database.")
 } else {
   logdriver::add_log(level = "info", event = "Consolidation: Success writing data to database.", username = username)
 }
